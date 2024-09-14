ba735f79fc9436b3eacf0173e074d5a74d12b94e
YagP <criper294@gmail.com>
2024-09-14 21:21:45 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace TELO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(249, 426);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "скипнуть перерыв";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Location = new System.Drawing.Point(509, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(279, 426);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(351, 101);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(75, 23);
+            this.Button2.TabIndex = 4;
+            this.Button2.Text = "сделал";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(351, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "скипнул";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(351, 418);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(351, 392);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(74, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "Секундомер";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TELO
+{
+    public partial class Form1 : Form
+    {
+        String CurEx="placeholder";
+        int control = 0;
+        int countdown=20;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (control == 0)
+            {
+                String i = chose();
+                //i += "\r\n";
+                textBox1.Text += DateTime.Now.ToString("HH:mm:ss tt");
+                textBox1.Text += i;
+                CurEx = i;
+                control = 1;
+                timer1.Enabled = false; timer2.Enabled=false;
+                countdown = 20;
+            }
+        }
+        private String chose()
+        {
+            String ex="Чето сломалось";
+            Random rnd = new Random();
+            int i;
+            int forotj;
+            i=rnd.Next(17);
+            forotj = rnd.Next(3)+1;
+
+            switch (i)
+            {
+                case 0:
+                    ex = "Отжимания Трицепс 20 ";
+                    ex += forotj;
+                    break;
+                case 1:
+                    ex = "Приседания 20";
+                    break;
+                case 2:
+                    ex = "Скручивания полные 20";
+                    break;
+                case 3:
+                    ex = "Отжимания Грудь 20 ";
+                    ex += forotj;
+                    break;
+                case 4:
+                    ex = "Отжимания Спина 20 ";
+                    ex += forotj/2;
+                    break;
+                case 5:
+                    ex = "Отжимания Плечи 20 ";
+                    ex += forotj;
+                    break;
+                case 6:
+                    ex = "Приседания с гантелью 10";
+                    break;
+                case 7:
+                    ex = "Скручивания неполные 30";
+                    break;
+                case 8:
+                    ex = "Плечи 30кг 5 раз";
+                    break;
+                case 9:
+                    ex = "Ножки уголком 35с";
+                    break;
+                case 10:
+                    ex = "Гига березка 30с";
+                    break;
+                case 11:
+                    ex = "Эспандер 30с";
+                    break;
+                case 12:
+                    ex = "Планка боковая 35с";
+                    break;
+                case 13:
+                    ex = "Скручивания вбок 36";
+                    break;
+                case 14:
+                    ex = "Бицепс 30кг 5";
+                    break;
+                case 15:
+                    ex = "Планка 69";
+                    break;
+                case 16:
+                    ex = "Планка на вытянутых 46";
+                    break;
+
+            }
+            return ex;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (control == 1)
+            {
+                String Krasivo = DateTime.Now.ToString("HH:mm:ss tt");
+                Krasivo += " ";
+                Krasivo += CurEx;
+                Krasivo += "\r\n";
+                textBox2.Text += Krasivo;
+                textBox1.Text += " ✔\r\n";
+                control = 0;
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (control == 1)
+            {
+  
+                textBox1.Text += " X\r\n";
+                control = 0;
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            button1_Click(sender, e);
+            timer1.Enabled = false;
+            countdown = 20;
+            String str = countdown.ToString();
+            textBox3.Text=str;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            countdown -= 1;
+            String str=countdown.ToString();
+            textBox3.Text=str;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
